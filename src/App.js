44579c850f780a6ba6fd0f0c479534c46c14ac0d
Dev Inikhiya <88757383+devichand-stan@users.noreply.github.com>
2023-05-22 18:06:47 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+let options = {
+  // Pass your App ID here.
+  appId: "7a15a1b80fd243fdac1324a4b878363b",
+  // Set the channel name.
+  channel: "",
+  // Pass your temp token here.
+  token: "",
+  // Set the user ID.
+  uid: 0,
+};
+
+let channelParameters = {
+  // A variable to hold a local audio track.
+  localAudioTrack: null,
+  // A variable to hold a remote audio track.
+  remoteAudioTrack: null,
+  // A variable to hold the remote user id.
+  remoteUid: null,
+};
 
 function App() {
+  const [count, setCount] = useState(0);
+  const agoraEngine = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+  async function startBasicCall() {
+    // Create an instance of the Agora Engine
+
+    // Listen for the "user-published" event to retrieve an AgoraRTCRemoteUser object.
+    agoraEngine.on("user-published", async (user, mediaType) => {
+      // Subscribe to the remote user when the SDK triggers the "user-published" event.
+      await agoraEngine.subscribe(user, mediaType);
+      console.log("subscribe success");
+
+      // Subscribe and play the remote audio track.
+      if (mediaType == "audio") {
+        channelParameters.remoteUid = user.uid;
+        // Get the RemoteAudioTrack object from the AgoraRTCRemoteUser object.
+        channelParameters.remoteAudioTrack = user.audioTrack;
+        // Play the remote audio track.
+        channelParameters.remoteAudioTrack.play();
+      }
+
+      // Listen for the "user-unpublished" event.
+      agoraEngine.on("user-unpublished", (user) => {
+        console.log(user.uid + "has left the channel");
+      });
+    });
+  }
+
+  useEffect(() => {
+    startBasicCall();
+  }, []);
+
+  const joinChannel = async () => {
+    await agoraEngine.join(
+      options.appId,
+      options.channel,
+      options.token,
+      options.uid
+    );
+    console.log("join channel");
+    channelParameters.localAudioTrack =
+      await AgoraRTC.createMicrophoneAudioTrack();
+    // Publish the local audio track in the channel.
+    await agoraEngine.publish(channelParameters.localAudioTrack);
+  };
+
+  const leaveChannel = async () => {
+    // Destroy the local audio track.
+    await agoraEngine.leave();
+    // Destroy the local audio track.
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button onClick={joinChannel}>Join Channel</button>
+      <button onClick={leaveChannel}>Leave Channel</button>
+    </>
   );
 }
 
